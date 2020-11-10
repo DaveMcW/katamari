1d7cdb0200056a2cb8e087fc0b63980c36b65ef1
David McWilliams <davemcw@gmail.com>
2020-11-10 01:57:40 -0500

    improve string format

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -1,12 +1,16 @@
 for i, radius in pairs(require("config.radius")) do
+  local diameter = string.format("%.1f", 2*radius)
+  if radius > 4.75 then
+    diameter = string.format("%.0f", 2*radius)
+  end
   local item = {
     type = "item-with-entity-data",
     name = "katamari-"..i,
     localised_name = {"entity-name.katamari"},
-    localised_description = {"item-description.katamari", string.format("%.1f", 2*radius)},
+    localised_description = {"item-description.katamari", diameter},
     place_result = "katamari-"..i,
     subgroup = "transport",
-    order = "b[personal-transport]-k[katamari]",
+    order = "b[personal-transport]-k[katamari]-" .. string.format("%.2d", i),
     icon = "__katamari__/graphics/icon.png",
     icon_mipmaps = 4,
     icon_size = 64,
